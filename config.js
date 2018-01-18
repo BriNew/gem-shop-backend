a990d818b5b2af2d7aa94b8762eb77104e2f86ec
Brian Newell <brian.newell@gmail.com>
2018-01-18 13:32:53 -0800

    connected to mlab

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+const mlabDbUrl =  "mongodb://BriNew:133Finnish@ds137206.mlab.com:37206/rock-inventory";
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/rock-inventory';
+                       mlabDbUrl;
 exports.PORT = process.env.PORT || 8080;
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://young-eyrie-40144.herokuapp.com';
 
 
