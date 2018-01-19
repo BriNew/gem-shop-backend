65143f9f430202899e3f57a7c4d536fe4c9f8c36
Brian Newell <brian.newell@gmail.com>
2018-01-19 13:55:45 -0800

    config

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        mlabDbUrl;
 exports.PORT = process.env.PORT || 'https://peaceful-reef-90796.herokuapp.com/';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://young-eyrie-40144.herokuapp.com/' || 'localhost:3000' ;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'localhost:3000' ;
 
 
