1d78da04ad41af363a0d8ef54d3c36626fe4588c
Brian Newell <brian.newell@gmail.com>
2018-01-19 13:57:11 -0800

    config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mlabDbUrl =  "mongodb://BriNew:133Finnish@ds137206.mlab.com:37206/rock-inv
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        mlabDbUrl;
-exports.PORT = process.env.PORT || 'https://peaceful-reef-90796.herokuapp.com/';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'localhost:3000' ;
+exports.PORT = process.env.PORT || 8080;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
 
