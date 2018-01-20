0a1394b9d452f3a6ee6bede7fdf06f9faf9e0904
Brian Newell <brian.newell@gmail.com>
2018-01-19 19:30:01 -0800

    config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        mlabDbUrl;
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 
-						clientURL;
-						// 'http://localhost:3000';
+						// clientURL;
+						'http://localhost:3000';
 
 
