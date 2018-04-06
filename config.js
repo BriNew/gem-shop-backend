c31ef887f2891ae0e9b7445da748463a79deb7e8
Brian Newell <brian.newell@gmail.com>
2018-04-06 09:05:44 -0500

    changed clientURL for CORS to custom domain

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const mlabDbUrl =  "mongodb://BriNew:133Finnish@ds137206.mlab.com:37206/rock-inventory";
-const clientURL = "https://young-eyrie-40144.herokuapp.com";
+const clientURL = "http://www.rockshopdemo.com/";
 
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
